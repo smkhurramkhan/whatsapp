c8c9847c4a7d01cd703b085ca26d2e48fe462b3c
S M Khurram Khan <smkhurramkhan@gmail.com>
2022-10-31 17:40:26 +0500

    chat contacts activity and adapter converted

--- app/src/main/java/com/iceka/whatsappclone/ContactActivity.kt
+++ app/src/main/java/com/iceka/whatsappclone/ContactActivity.kt
@@ -1,113 +1,76 @@
-package com.iceka.whatsappclone;
+package com.iceka.whatsappclone
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.DefaultItemAnimator;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import android.os.Bundle
+import android.view.Menu
+import android.view.accessibility.AccessibilityEvent
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
+import com.iceka.whatsappclone.adapters.ContactAdapter
+import com.iceka.whatsappclone.models.User
 
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.View;
-import android.view.accessibility.AccessibilityEvent;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
-import com.iceka.whatsappclone.adapters.ContactAdapter;
-import com.iceka.whatsappclone.models.User;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class ContactActivity extends AppCompatActivity {
-
-    private FirebaseAuth mFirebaseAuth;
-    private FirebaseDatabase mFirebaseDatabase;
-    private DatabaseReference mDatabaseReference;
-    private FirebaseUser mFirebaseUser;
-
-    private RecyclerView mRecyclerView;
-    private Toolbar mToolbar;
-    private TextView mContactCount;
-
-    private ContactAdapter mAdapter;
-
-    private List<User> contactList = new ArrayList<>();
-
-    @Override
-    public boolean dispatchPopulateAccessibilityEvent(AccessibilityEvent event) {
-        return super.dispatchPopulateAccessibilityEvent(event);
+class ContactActivity : AppCompatActivity() {
+    private var mDatabaseReference: DatabaseReference? = null
+    private var mFirebaseUser: FirebaseUser? = null
+    private var mRecyclerView: RecyclerView? = null
+    private var mContactCount: TextView? = null
+    private var mAdapter: ContactAdapter? = null
+    private val contactList = mutableListOf<User>()
+    override fun dispatchPopulateAccessibilityEvent(event: AccessibilityEvent): Boolean {
+        return super.dispatchPopulateAccessibilityEvent(event)
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contact);
-
-        mRecyclerView = findViewById(R.id.recycler_view_contact);
-        mToolbar = findViewById(R.id.toolbar_contact);
-        mContactCount = findViewById(R.id.tv_contact_counts);
-
-        setSupportActionBar(mToolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-
-        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
-
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
-        mRecyclerView.setLayoutManager(layoutManager);
-        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
-
-        mFirebaseDatabase = FirebaseDatabase.getInstance();
-        mFirebaseAuth = FirebaseAuth.getInstance();
-        mFirebaseUser = mFirebaseAuth.getCurrentUser();
-        mDatabaseReference = mFirebaseDatabase.getReference().child("users");
-
-        getData();
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact)
+        mRecyclerView = findViewById(R.id.recycler_view_contact)
+        val mToolbar = findViewById<Toolbar>(R.id.toolbar_contact)
+        mContactCount = findViewById(R.id.tv_contact_counts)
+        setSupportActionBar(mToolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        mToolbar.setNavigationOnClickListener { finish() }
+        val layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(applicationContext)
+        mRecyclerView?.layoutManager = layoutManager
+        mRecyclerView?.itemAnimator = DefaultItemAnimator()
+        val mFirebaseDatabase = FirebaseDatabase.getInstance()
+        val mFirebaseAuth = FirebaseAuth.getInstance()
+        mFirebaseUser = mFirebaseAuth.currentUser
+        mDatabaseReference = mFirebaseDatabase.reference.child("users")
+        data
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_contact, menu);
-        return true;
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_contact, menu)
+        return true
     }
 
-    private void getData() {
-        Query myQuery = mDatabaseReference.orderByChild("username");
-        myQuery.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                contactList.clear();
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    User user = snapshot.getValue(User.class);
-                    if (!user.getUid().equals(mFirebaseUser.getUid())) {
-                        contactList.add(user);
-                        mContactCount.setText(String.valueOf(contactList.size()) + " contacts");
+    private val data: Unit
+         get() {
+            val myQuery = mDatabaseReference!!.orderByChild("username")
+            myQuery.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    contactList.clear()
+                    for (snapshot in dataSnapshot.children) {
+                        val user = snapshot.getValue(
+                            User::class.java
+                        )
+                        if (user?.uid != mFirebaseUser?.uid) {
+                            user?.let { contactList.add(it) }
+                            mContactCount?.text = contactList.size.toString() + " contacts"
+                        }
                     }
-
+                    mAdapter = ContactAdapter(this@ContactActivity, contactList)
+                    mRecyclerView?.adapter = mAdapter
                 }
-                mAdapter = new ContactAdapter(ContactActivity.this, contactList);
-                mRecyclerView.setAdapter(mAdapter);
-            }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-    }
+                override fun onCancelled(databaseError: DatabaseError) {}
+            })
+        }
 }
--- app/src/main/java/com/iceka/whatsappclone/adapters/ContactAdapter.kt
+++ app/src/main/java/com/iceka/whatsappclone/adapters/ContactAdapter.kt
@@ -1,127 +1,72 @@
-package com.iceka.whatsappclone.adapters;
+package com.iceka.whatsappclone.adapters
 
-import android.content.Context;
-import android.content.Intent;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.iceka.whatsappclone.ChatRoomActivity
+import com.iceka.whatsappclone.R
+import com.iceka.whatsappclone.models.User
+import de.hdodenhof.circleimageview.CircleImageView
 
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.bumptech.glide.Glide;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.iceka.whatsappclone.ChatRoomActivity;
-import com.iceka.whatsappclone.R;
-import com.iceka.whatsappclone.models.User;
-
-import java.util.List;
-
-import de.hdodenhof.circleimageview.CircleImageView;
-
-public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.MyViewHolder> {
-
-    private List<User> contactList;
-    private Context mContext;
-
-    public ContactAdapter(Context context, List<User> contacts) {
-        this.contactList = contacts;
-        this.mContext = context;
+class ContactAdapter(private val mContext: Context, private val contactList: List<User>) :
+    RecyclerView.Adapter<ContactAdapter.MyViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_contact_list, parent, false)
+        return MyViewHolder(view)
     }
 
-    @NonNull
-    @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_contact_list, parent, false);
-        return new MyViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull final MyViewHolder holder, final int position) {
-        final User currentUser = contactList.get(position);
-
-        DatabaseReference userReference = FirebaseDatabase.getInstance().getReference().child("users");
-        userReference.child(currentUser.getUid()).addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                final User user = dataSnapshot.getValue(User.class);
-                holder.mContactItem.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Intent intent = new Intent(mContext, ChatRoomActivity.class);
-                        intent.putExtra(ChatRoomActivity.EXTRAS_USER, user);
-                        intent.putExtra("userUid", currentUser.getUid());
-                        mContext.startActivity(intent);
-//                        Toast.makeText(mContext, "last : " + user.getLastSeen(), Toast.LENGTH_LONG).show();
-                    }
-                });
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val currentUser = contactList[position]
+        val userReference = FirebaseDatabase.getInstance().reference.child("users")
+        userReference.child(currentUser.uid).addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val user = dataSnapshot.getValue(
+                    User::class.java
+                )
+                holder.mContactItem.setOnClickListener {
+                    val intent = Intent(mContext, ChatRoomActivity::class.java)
+                    intent.putExtra(ChatRoomActivity.EXTRAS_USER, user)
+                    intent.putExtra("userUid", currentUser.uid)
+                    mContext.startActivity(intent)
+                }
             }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-//                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-
-//                final User user = dataSnapshot.getValue(User.class);
-//                holder.mTvUsername.setText(user.getUsername());
-//                holder.mContactItem.setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View view) {
-//                        Intent intent = new Intent(mContext, ChatRoomActivity.class);
-//                        intent.putExtra(ChatRoomActivity.EXTRAS_USER, user);
-//                        intent.putExtra("userUid", currentUser.getUid());
-////                        mContext.startActivity(intent);
-////                        Toast.makeText(mContext, "username : " + user.getUsername(), Toast.LENGTH_SHORT).show();
-//                    }
-//                });
-//                Toast.makeText(mContext, "tes : " + user.getUsername(), Toast.LENGTH_SHORT).show();
-
-
-
-        holder.mTvUsername.setText(currentUser.getUsername());
-        holder.mTvAbout.setText(currentUser.getAbout());
-        holder.mAvatar.setVisibility(View.VISIBLE);
-        Glide.with(holder.mAvatar.getContext())
-                .load(currentUser.getPhotoUrl())
-                .into(holder.mAvatar);
-//        holder.mContactItem.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                User user = contactList.get(position);
-//                Intent intent = new Intent(mContext, ChatRoomActivity.class);
-//                intent.putExtra("userUid", user.getUid());
-//                mContext.startActivity(intent);
-//            }
-//        });
+            override fun onCancelled(databaseError: DatabaseError) {}
+        })
+        holder.mTvUsername.text = currentUser.username
+        holder.mTvAbout.text = currentUser.about
+        holder.mAvatar.visibility = View.VISIBLE
+        Glide.with(holder.mAvatar.context)
+            .load(currentUser.photoUrl)
+            .into(holder.mAvatar)
     }
 
-    @Override
-    public int getItemCount() {
-        return contactList.size();
+    override fun getItemCount(): Int {
+        return contactList.size
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
-        private TextView mTvUsername;
-        private TextView mTvAbout;
-        private CircleImageView mAvatar;
-        private RelativeLayout mContactItem;
+    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val mTvUsername: TextView
+        val mTvAbout: TextView
+        val mAvatar: CircleImageView
+        val mContactItem: RelativeLayout
 
-        public MyViewHolder(@NonNull View itemView) {
-            super(itemView);
-            mTvUsername = itemView.findViewById(R.id.tv_username_contact);
-            mTvAbout = itemView.findViewById(R.id.tv_about_contact);
-            mAvatar = itemView.findViewById(R.id.avatar_user_contact);
-            mContactItem = itemView.findViewById(R.id.item_contact_layout);
+        init {
+            mTvUsername = itemView.findViewById(R.id.tv_username_contact)
+            mTvAbout = itemView.findViewById(R.id.tv_about_contact)
+            mAvatar = itemView.findViewById(R.id.avatar_user_contact)
+            mContactItem = itemView.findViewById(R.id.item_contact_layout)
         }
-
     }
 }
