e08089196388617b9d60eeec695f7b7ee4dce3a3
S M Khurram Khan <smkhurramkhan@gmail.com>
2022-11-02 17:02:03 +0500

    webview fixed

--- app/src/main/java/com/iceka/whatsappclone/weather/WeatherWebview.kt
+++ app/src/main/java/com/iceka/whatsappclone/weather/WeatherWebview.kt
@@ -3,10 +3,12 @@ package com.iceka.whatsappclone.weather
 import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.os.Bundle
+import android.view.View
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import com.airbnb.lottie.LottieAnimationView
 import com.iceka.whatsappclone.R
 
 class WeatherWebview : AppCompatActivity()
@@ -14,10 +16,12 @@ class WeatherWebview : AppCompatActivity()
 {
 
     private lateinit var webView: WebView
+    private lateinit var lottieJson: LottieAnimationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weather_webview)
+        lottieJson = findViewById(R.id.lottie)
         webView = findViewById(R.id.webview)
         webView.settings.javaScriptEnabled = true
         loadWebView("https://weatherwalay.com/tvas")
@@ -39,11 +43,16 @@ class WeatherWebview : AppCompatActivity()
             override fun onPageFinished(view: WebView, url: String) {
                 super.onPageFinished(view, url)
                 //dialog?.dismiss()
+                webView.visibility = View.VISIBLE
+                lottieJson.visibility = View.GONE
+
             }
 
             override fun onPageStarted(view: WebView, url: String, favicon: Bitmap?) {
                 super.onPageStarted(view, url, favicon)
                // dialog?.show()
+                webView.visibility = View.GONE
+                lottieJson.visibility = View.VISIBLE
             }
         }
     }
